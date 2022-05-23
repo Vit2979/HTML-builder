d3e40f9b1d3f818968d2150eb47492195b8632e5
Vit2979 <86978620+Vit2979@users.noreply.github.com>
2022-05-24 01:20:27 +0300

    feat: add basic (Tue, May 24, 2022 1:20:15 AM)

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,45 @@
+const fs = require('fs');
+const path = require('path');
+
+const folderDirection = path.join(__dirname, '/files');
+const folderTransferDirection = path.join(__dirname, '/files-copy');
+
+const folderTransfer = (folderDirection, folderTransferDirection) => {
+  fs.rm(folderTransferDirection, { recursive: true, force: true }, (n) => {
+    if (n) {
+      return console.error(n);
+    }
+
+    fs.mkdir(folderTransferDirection, { recursive: true }, (n) => {
+      if (n) {
+        return console.error(n);
+      }
+    });
+
+    fs.readdir(folderDirection, { withFileTypes: true }, (n, elements) => {
+      if (n) {
+        return console.error(n);
+      }
+
+      elements.forEach((element) => {
+        const fileDirection = path.join(folderDirection, element.name);
+        const fileTransferDirection = path.join(folderTransferDirection, element.name);
+
+        if (element.isFile()) {
+          fs.copyFile(fileDirection, fileTransferDirection, (n) => {
+            if (n) {
+              return console.error(n);
+            }
+          });
+        }
+
+        if (element.isDirectory()) {
+          folderTransfer(fileDirection, fileTransferDirection);
+        }
+      });
+    });
+  });
+};
+
+folderTransfer(folderDirection, folderTransferDirection);
 
